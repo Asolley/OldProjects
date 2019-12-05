1a71326626cc7c90c96a3449a4c6b5a6b5c5294d
Asolley <ansolley@gmail.com>
2019-12-05 16:31:00 -0500

    Old Project

---
+++ Solley_Andrew_Xelda/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+
+    Transform player;
+    Rigidbody2D myRigidbody;
+    Vector3 destination;
+    public enum PrimaryBehavior { wander, patrol }
+    public enum SecondaryBehaviors { chase, evade }
+    public enum States { chasing, evading, patrolling, wandering, waiting }
+    public PrimaryBehavior primaryBehavior;
+    public SecondaryBehaviors secondaryBehavior;
+    States originalState;
+    States currentState;
+    float timeBetweenMoveCounter;
+    public float timeBetwennMove;
+    public float moveSpeed;
+    public float wanderRange;
+    public Transform[] wayPoints;
+    int wayPointIndex;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player").GetComponent<Transform>();
+        destination = transform.position;
+        if (primaryBehavior == PrimaryBehavior.wander)
+        {
+            originalState = States.wandering;
+        }
+        else if (primaryBehavior == PrimaryBehavior.patrol)
+        {
+            originalState = States.patrolling;
+        }
+        currentState = originalState;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("My current state is" + currentState);
+        if (currentState == States.wandering)
+        {
+            Wander();
+        }
+        else if (currentState == States.waiting)
+        {
+            Wait();
+        }
+        else if (currentState == States.chasing)
+        {
+            Chase(player.position);
+        }
+        else if (currentState == States.evading)
+        {
+            Evade();
+        }
+        else if (currentState == States.patrolling)
+        {
+            Partol();
+        }
+    }
+    void Wander()
+    {
+        if (transform.position == destination)
+        {
+            currentState = States.waiting;
+            destination = RandomDestination();
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, moveSpeed * Time.deltaTime);
+        }
+    }
+
+    Vector3 RandomDestination()
+    {
+        Vector3 newDestination;
+        newDestination = new Vector3(0 + Random.Range(-wanderRange, wanderRange), 1 + Random.Range(-wanderRange, wanderRange));
+        return newDestination;
+    }
+    void Wait()
+    {
+        myRigidbody.velocity = Vector3.zero;
+        timeBetweenMoveCounter -= Time.deltaTime;
+        if (timeBetweenMoveCounter < 0)
+        {
+            timeBetweenMoveCounter = timeBetwennMove;
+            currentState = originalState;
+        }
+    }
+    void Chase(Vector3 target)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target) < 0.01f)
+        {
+            currentState = States.waiting;
+        }
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.name == player.name)
+        {
+            if (secondaryBehavior == SecondaryBehaviors.chase)
+            {
+                currentState = States.chasing;
+            }
+            else if (secondaryBehavior == SecondaryBehaviors.evade)
+            {
+                currentState = States.evading;
+            }
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == player.name)
+        {
+            currentState = States.waiting;
+        }
+    }
+    void OnCollisionStay2D(Collision2D other)
+    {
+        myRigidbody.simulated = false;      
+        currentState = States.waiting;
+    }
+    void OnCollisionExit2D(Collision2D other)
+    {
+        myRigidbody.simulated = true;       
+    }
+    void Evade()
+    {
+        Vector3 direction = transform.position - player.position;
+        transform.Translate(direction.normalized * moveSpeed * Time.deltaTime);
+    }
+    void Partol()
+    {
+        if (transform.position == destination)
+        {
+            wayPointIndex = wayPointIndex + 1;
+            if (wayPointIndex == wayPoints.Length)
+            {
+                wayPointIndex = 0;
+            }
+            destination = wayPoints[wayPointIndex].position;
+            currentState = States.waiting;
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, moveSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Solley_Andrew_Xelda/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rBody;
+    Animator anim;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (movement_vector != Vector2.zero && anim.GetBool("isAttacking")== false)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetFloat("input_x", movement_vector.x);
+            anim.SetFloat("input_y", movement_vector.y);
+            rBody.MovePosition(rBody.position + movement_vector * Time.deltaTime);
+        }
+
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+        if (Input.GetButton("Fire1"))
+        {
+            anim.SetBool("isAttacking", true);
+            anim.SetBool("isWalking", false);
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            anim.SetBool("isAttacking", false);
+        }
+      }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (anim.GetBool("isAttacking"))
+        {
+            Destroy(other.gameObject);
+        }
+
+    }
+   }
