dc7ecad46155c717964dd9e8229b6cd2710db549
Asolley <ansolley@gmail.com>
2019-12-05 16:32:41 -0500

    Old Project

---
+++ Solley_Andrew_MenuSystem/Assets/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void LoadLevel(string name)
+    {
+        Debug.Log("level load requested for:" + name);
+        Application.LoadLevel(name);
+        SceneManager.LoadScene(name);
+    }
+
+}
